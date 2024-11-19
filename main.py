117ba736650f1b4445b5f1747e45e2f9412a427e
elipotter2009 <elipotter09@gmail.com>
2024-11-19 09:36:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-
+Print ('hello ' ,what, 'would you like')
+print ('we ' ,have, tomatoes (+$1.50), onions (+$1.25), pineapple (+$3.50), mushrooms (+$3.75), avocado (+$0.40))
+print ('your ' ,you, chose mushrooms (+$3.75), along side onions(+$1.25) witch comes out to ($5)
