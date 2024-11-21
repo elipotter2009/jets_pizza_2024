e851a014a89f29525d868e7bde8d9551e488a3d4
elipotter2009 <elipotter09@gmail.com>
2024-11-21 09:19:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,47 @@ def pizza_price():
 
 base_price = 15
 topping_price = 0
-# tomatoes (+1.50).
-# onions (+1.25).
-# pineapple (+3.50).
-# mushrooms (+3.75), and 
-# avocado (+0.40).
+calculated_topping =[]
+
+for topping in requested_toppimg:
+  continue
+
+
+if topping in calculated_topping
+continue
 
 for topping in requested_topping:
-  
+
+  if topping == "T":
+    toppings_price += 1.5
+
+ if topping == "O":
+   toppings_price += 1.25
+
+ if topping == "P":
+   topping_price += 3.5
+
+ if topping == "M":
+   topping_price += 3.75
+
+ if topping == "A":
+    toppings_price += .40
+
+    total_price = base_price + topping_price
+   if total_price > 20:
+     total_price * .95
+
+print(total_price)
+
+total_price = Round(total_price, 2)
+
+print(total_price)
+
+pizza_price()
+
+
+
+
+
+
+
